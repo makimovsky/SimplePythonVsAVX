799180ee9ddacb34061937fc4e31b5f78f1ab409
makimovsky <makimofficial@gmail.com>
2024-02-24 14:21:44 +0100

    Zbudowanie modelu wykorzystując bibliotekę TensorFlow. Dokładność modelu dla danych testowych ~90% (zalando fashion-mnist dataset)

---
+++ src/CNNmodel.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+
+from tensorflow.keras import layers, datasets, models
+
+# pobranie zbioru danych do uczenia i testowania
+(train_images, train_labels), (test_images, test_labels) = datasets.fashion_mnist.load_data()
+
+# normalizacja wartosci pikseli
+train_images, test_images = train_images / 255.0, test_images / 255.0
+
+# utworzenie modelu
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(layers.MaxPooling2D((2, 2)))
+
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10))
+
+# model.summary()
+
+# kompilacja i trenowanie modelu
+model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=10, validation_data=(test_images, test_labels))
+
+# dokładność modelu
+score = model.evaluate(test_images,  test_labels, return_dict=True)
+
+print("\nDokładność modelu dla danych testowych:", score["accuracy"])
