800f1cb94f062948e456dd48cfa47ba4025bb898
makimovsky <makimofficial@gmail.com>
2024-03-30 12:10:12 +0100

    Wygenerowanie modelu w C

--- src/c/main.c
+++ src/c/main.c
@@ -1,29 +1,30 @@
+#include <stdio.h>
 #include "CNNModelAVX.h"
 
 int main(int argc, char** argv) {
     //Wczytanie ParamsFile
-    size_t size = sizeof(CNNModelAVXParams);
-    CNNModelAVXParams* params = malloc(size);
+    size_t sizeNet = sizeof(CNNModelAVXParams);
+    CNNModelAVXParams* params = malloc(sizeNet);
     FILE* from = fopen("../../resources/ParamsFile", "r");
-    fread(params, size, 1, from);
+    fread(params, sizeNet, 1, from);
     fclose(from);
 
     //Utworzenie siatki
     CNNModelAVXNet* net;
-    char* err = CNNModelAVXNetCreate(&net, params, 4);
+    char* errNet = CNNModelAVXNetCreate(&net, params, 4);
     free(params);
-    if (err) {
-        printf("%s\n", err);
-        free(err);
+    if (errNet) {
+        printf("%s\n", errNet);
+        free(errNet);
         exit(1); 
     }
 
     //Utworzenie silnika
     CNNModelAVXEngine* engine;
-    char* err = CNNModelAVXEngineCreate(&engine, net, 4);
-    if (err) {
-        printf("%s\n", err); 
-        free(err); 
+    char* errEngine = CNNModelAVXEngineCreate(&engine, net, 4);
+    if (errEngine) {
+        printf("%s\n", errEngine); 
+        free(errEngine); 
         CNNModelAVXNetDestroy(net);
         exit(1); 
     }
@@ -33,10 +34,10 @@ int main(int argc, char** argv) {
     //Wczytanie obrazu i wynik
     float* inputdataData = malloc(sizeof(float)*1*28*28);
     float* outputlogitsData = malloc(sizeof(float)*10*1*1);
-    size_t size = sizeof(inputdataData);
+    size_t sizeData = sizeof(inputdataData);
 
-    FILE* image = fopen("../../resources/image0", "r")
-    fread(inputdataData, size, 1, image)
+    FILE* image = fopen("../../resources/image0", "r");
+    fread(inputdataData, sizeData, 1, image);
 
     CNNModelAVXEngineInference(
         engine,
