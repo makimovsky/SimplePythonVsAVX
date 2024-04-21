e053971b85e68175ed4f3cccc2c640003d39a071
makimovsky <makimofficial@gmail.com>
2024-04-21 13:10:22 +0200

    Plik ParamsFile22 - tylko splaszczenie

--- src/Python/ParamsFileCreator.py
+++ src/Python/ParamsFileCreator.py
@@ -25,11 +25,11 @@ print(output_logits_weights.shape)
 # struktura ParamsFile
 cnn_params = {
     'convoutputBiases': conv_output_biases,
-    'convoutputWeights': tf.transpose(conv_output_weights, perm=[3, 2, 0, 1]).numpy().flatten(),
+    'convoutputWeights': conv_output_weights.flatten(),
     'denseoutputBiases': dense_output_biases,
-    'denseoutputWeights': tf.transpose(dense_output_weights, perm=[1, 0]).numpy().flatten(),
+    'denseoutputWeights': dense_output_weights.flatten(),
     'outputlogitsBiases': output_logits_biases,
-    'outputlogitsWeights': tf.transpose(output_logits_weights, perm=[1, 0]).numpy().flatten(),
+    'outputlogitsWeights': output_logits_weights.flatten(),
 }
 
 print("\nPo przekształceniu:")
@@ -41,7 +41,7 @@ print(cnn_params.get("outputlogitsBiases").shape)
 print(cnn_params.get("outputlogitsWeights").shape)
 
 # zapisanie binarne wag do pliku ParamsFile
-with open("../../resources/ParamsFile21", "wb") as file:
+with open("../../resources/ParamsFile22", "wb") as file:
     for key, value in cnn_params.items():
         value.tofile(file)
 
