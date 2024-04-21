68609177900caa11aa981b468bcc666c38c29adc
makimovsky <makimofficial@gmail.com>
2024-04-21 13:06:55 +0200

    Plik ParamsFile21 - transpose 3,2,0,1 zamiast 3,2,1,0

--- src/Python/ParamsFileCreator.py
+++ src/Python/ParamsFileCreator.py
@@ -25,7 +25,7 @@ print(output_logits_weights.shape)
 # struktura ParamsFile
 cnn_params = {
     'convoutputBiases': conv_output_biases,
-    'convoutputWeights': tf.transpose(conv_output_weights, perm=[3, 2, 1, 0]).numpy().flatten(),
+    'convoutputWeights': tf.transpose(conv_output_weights, perm=[3, 2, 0, 1]).numpy().flatten(),
     'denseoutputBiases': dense_output_biases,
     'denseoutputWeights': tf.transpose(dense_output_weights, perm=[1, 0]).numpy().flatten(),
     'outputlogitsBiases': output_logits_biases,
@@ -41,7 +41,7 @@ print(cnn_params.get("outputlogitsBiases").shape)
 print(cnn_params.get("outputlogitsWeights").shape)
 
 # zapisanie binarne wag do pliku ParamsFile
-with open("../../resources/ParamsFile2", "wb") as file:
+with open("../../resources/ParamsFile21", "wb") as file:
     for key, value in cnn_params.items():
         value.tofile(file)
 
