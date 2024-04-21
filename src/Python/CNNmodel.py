90613520a6d5ac84198689f1312e48856f3ea854
makimovsky <makimofficial@gmail.com>
2024-04-21 13:00:17 +0200

    Zmiana na format NCHW przy tworzeniu modelu.

--- src/Python/CNNmodel.py
+++ src/Python/CNNmodel.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-
 from tensorflow.keras import layers, datasets, models
 
 # pobranie zbioru danych do uczenia i testowania
@@ -8,16 +7,18 @@ from tensorflow.keras import layers, datasets, models
 # normalizacja wartosci pikseli
 train_images, test_images = train_images / 255.0, test_images / 255.0
 
+# przekształcenie danych wejściowych na format NCHW
+train_images = train_images.reshape(train_images.shape[0], 1, 28, 28)
+test_images = test_images.reshape(test_images.shape[0], 1, 28, 28)
+
 # utworzenie modelu
 model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
-model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(1, 28, 28), data_format='channels_first'))
+model.add(layers.MaxPooling2D((2, 2), data_format='channels_first'))
 
 model.add(layers.Flatten())
 model.add(layers.Dense(64, activation='relu'))
 model.add(layers.Dense(10))
-
-# model.summary()
 
 # kompilacja i trenowanie modelu
 model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
@@ -28,7 +29,7 @@ model.fit(train_images, train_labels, epochs=10, validation_data=(test_images, t
 # dokładność modelu
 score = model.evaluate(test_images,  test_labels, return_dict=True)
 
-print("\nDokładność modelu dla danych testowych:", score["accuracy"])
+print("\nModel accuracy for test data:", score["accuracy"])
 
 # zapisanie modelu
-model.save('../../resources/model.keras')
+model.save('../../resources/model2.keras')
--- src/Python/ParamsFileCreator.py
+++ src/Python/ParamsFileCreator.py
@@ -1,12 +1,18 @@
 import tensorflow as tf
+# import h5py
+#
+# with h5py.File('../../resources/model.weights.h5', 'r') as f:
+#     print(list(f["layers\\conv2d"]["vars"]["0"].shape))
+
 
 # wczytanie zapisanego modelu
-model = tf.keras.models.load_model("../../resources/model.keras")
+model = tf.keras.models.load_model("../../resources/model2.keras")
 
 # wydobycie wag
 conv_output_weights, conv_output_biases = model.layers[0].get_weights()
 dense_output_weights, dense_output_biases = model.layers[3].get_weights()
 output_logits_weights, output_logits_biases = model.layers[4].get_weights()
+
 
 print("Przed przekształceniem:")
 print(conv_output_biases.shape)
@@ -19,11 +25,11 @@ print(output_logits_weights.shape)
 # struktura ParamsFile
 cnn_params = {
     'convoutputBiases': conv_output_biases,
-    'convoutputWeights': tf.transpose(conv_output_weights, perm=[0, 3, 1, 2]).numpy().flatten(),
+    'convoutputWeights': tf.transpose(conv_output_weights, perm=[3, 2, 1, 0]).numpy().flatten(),
     'denseoutputBiases': dense_output_biases,
-    'denseoutputWeights': tf.transpose(dense_output_weights, perm=[0, 1]).numpy().flatten(),
+    'denseoutputWeights': tf.transpose(dense_output_weights, perm=[1, 0]).numpy().flatten(),
     'outputlogitsBiases': output_logits_biases,
-    'outputlogitsWeights': tf.transpose(output_logits_weights, perm=[0, 1]).numpy().flatten(),
+    'outputlogitsWeights': tf.transpose(output_logits_weights, perm=[1, 0]).numpy().flatten(),
 }
 
 print("\nPo przekształceniu:")
@@ -35,7 +41,7 @@ print(cnn_params.get("outputlogitsBiases").shape)
 print(cnn_params.get("outputlogitsWeights").shape)
 
 # zapisanie binarne wag do pliku ParamsFile
-with open("../../resources/ParamsFile", "wb") as file:
+with open("../../resources/ParamsFile2", "wb") as file:
     for key, value in cnn_params.items():
         value.tofile(file)
 
